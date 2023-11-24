fix issue in rotation

examples/load_UWB_IMU_raw_data.py
@@ -7,9 +7,24 @@ import scipy.spatial.transform as sct
 from aitviewer.configuration import CONFIG as C
 from aitviewer.models.smpl import SMPLLayer
 from aitviewer.renderables.rigid_bodies import RigidBodies
+from aitviewer.renderables.arrows import Arrows
 from aitviewer.renderables.smpl import SMPLSequence
 from aitviewer.viewer import Viewer
 import pandas as pd
+
+uwb_imu_mapping = torch.tensor([1, 2, 4, 5, 3, 0])
+def _syn_acc(v, smooth_n=4):
+    r"""
+    Synthesize accelerations from vertex positions.
+    """
+    mid = smooth_n // 2
+    acc = torch.stack([(v[i] + v[i + 2] - 2 * v[i + 1]) * 3600 for i in range(0, v.shape[0] - 2)])
+    acc = torch.cat((torch.zeros_like(acc[:1]), acc, torch.zeros_like(acc[:1])))
+    if mid != 0:
+        acc[smooth_n:-smooth_n] = torch.stack(
+            [(v[i] + v[i + smooth_n * 2] - 2 * v[i + smooth_n]) * 3600 / smooth_n ** 2
+             for i in range(0, v.shape[0] - smooth_n * 2)])
+    return acc
 
 def load_csv(csv_file: str, resample = False):
     '''
@@ -36,7 +51,8 @@ def load_csv(csv_file: str, resample = False):
     device_key = "device"
     device_ids = [0,1,2,3,4,5]
     devices_id_val = set(data[device_key])
-    keys = ["time[ms]","x_gt","y_gt","z_gt","qx_gt","qy_gt","qz_gt","qw_gt"]
+    #keys = ["time[ms]","x_gt","y_gt","z_gt","qx_gt","qy_gt","qz_gt","qw_gt"]
+    keys = ["time","x","y","z","qx","qy","qz","qw"]
     output_data = []
     for dev_id in device_ids:
         if dev_id not in devices_id_val:
@@ -48,9 +64,12 @@ def load_csv(csv_file: str, resample = False):
 
         if resample:
             imu_df = device_data[keys]
-            imu_df = resample_time_series(imu_df, time_col="time[ms]",
-                                        val_col=["x_gt","y_gt","z_gt","qx_gt","qy_gt","qz_gt","qw_gt"],
-                                        desired_time_stamp="33ms")
+            # imu_df = resample_time_series(imu_df, time_col="time[ms]",
+            #                             val_col=["x_gt","y_gt","z_gt","qx_gt","qy_gt","qz_gt","qw_gt"],
+            #                             desired_time_stamp="16.667ms")
+            imu_df = resample_time_series(imu_df, time_col="time",
+                                        val_col=["x","y","z","qx","qy","qz","qw"],
+                                        desired_time_stamp="16.667ms")
         else:
             imu_df = device_data[keys[1:]]
             
@@ -61,40 +80,69 @@ def load_csv(csv_file: str, resample = False):
     
     return output_data.transpose(1,0,2) #frame, device, dim
 
-def get_valid_transformation(rotmat, position):
+def get_valid_transformation(imu_ori, imu_acc):
     '''
     Get ori,tran for imu in smpl coordinate frame to build rigid body
     '''
-    uwb_rot = torch.tensor([[0, 1, 0], [0, 0, 1.0], [1, 0, 0]]) 
+    uwb_rot = torch.tensor([[0, 1, 0], [0, 0, 1.0], [1, 0, 0]])
+    offset = torch.tensor([[1, 0, 0], [0, 1, 0], [0, 0, 1.0]]) 
+    uwb_rot_i = offset.matmul(uwb_rot)
     
-    imu_ori = torch.from_numpy(rotmat).float()
     imu_ori = torch.einsum('ij,abjk->abik',uwb_rot, imu_ori)
+    imu_acc = torch.einsum('ij,abj->abi',uwb_rot, imu_acc)
     
-    imu_position = torch.from_numpy(position).float()
-    imu_position = torch.einsum('ij,abj->abi',uwb_rot, imu_position)
+    return imu_ori, imu_acc
+
+def process_imu_ori(imu_ori: torch.Tensor, align_init_ori = False):
+    #Our assumption
+    init_root_ori = torch.eye(3)
+    #get target imu ori at initial
+    if align_init_ori:
+        #align all ori at time 0 to be identity, assume identical ori for all imu (syn)
+        init_imu_target = torch.tensor([[1, 0, 0], [0, 1, 0], [0, 0, 1.0]]).tile(1,1)
+    else:
+        #only assume root orientation to be identity, asuume root ori is identity, other imu is not identical to root(real)
+        init_imu_target = init_root_ori @ imu_ori[[0],[0],...].unsqueeze(0).permute(0,1,3,2) @ imu_ori[[0],...]
+        
+    init_imu_ori = imu_ori[[0],...]
+    #offset at time 0
+    cal_mat_offset = init_imu_target.matmul(init_imu_ori.permute(0,1,3,2))
+    #Rotation from time 0 to time t
+    rot_0_t = imu_ori.matmul(init_imu_ori.permute(0,1,3,2))
+    #offset at time t
+    cal_mat_offset = rot_0_t @ cal_mat_offset @ rot_0_t.permute(0,1,3,2) 
     
-    return imu_ori, imu_position
+    imu_ori = cal_mat_offset.matmul(imu_ori) 
 
-
-
+    return imu_ori
 
 if __name__ == "__main__":
-    csv_file_path = "/home/chqian/DATA/Motion_capture_raw_dataset/Dataset_B/subject_4/freestyle/04_freestyle_devices_0/dataset_imu.csv"
+    #csv_file_path = "/home/chqian/DATA/Motion_capture_raw_dataset/Dataset_B/subject_4/freestyle/04_freestyle_devices_0/dataset_imu.csv"
+    csv_file_path = "/home/chqian/DATA/UWB_test_data/20231121_markers_tests-20231122T092610Z-001/20231121_markers_tests/aligned_dense_markers.csv"
     output_data = load_csv(csv_file_path,resample=True)
     frame_num, device_num, dim = output_data.shape
+    
     pos_device = output_data[:,:,:3]
     device_ori = output_data[:,:,3:]
     rotmat_device = sct.Rotation.from_quat(device_ori.reshape(-1,4)).as_matrix().reshape(frame_num,device_num,3,3)
     
-    imu_ori,imu_pos = get_valid_transformation(rotmat_device, pos_device)
+    #Transform to smpl space (z_up to y_up)
+    imu_ori = torch.from_numpy(rotmat_device).float()
+    imu_pos = torch.from_numpy(pos_device).float()
+    imu_ori,imu_pos = get_valid_transformation(imu_ori,imu_pos)
+    imu_acc = _syn_acc(imu_pos)
     
+    imu_ori = process_imu_ori(imu_ori,align_init_ori=True)
+
     rb = RigidBodies(imu_pos.cpu().numpy(), imu_ori.cpu().numpy(),color=(0,0,0,1))
-
-
-    # Add everything to the scene and display at 30 fps.
+    arr = Arrows(origins=imu_pos.cpu().numpy(), tips=imu_pos.cpu().numpy() + 0.01 * imu_acc.cpu().numpy())
+    frame_num = imu_ori.size(0)
+    torch.save({'acc': [imu_acc[:,uwb_imu_mapping]], 'ori': [imu_ori[:,uwb_imu_mapping]], 'pose': [torch.zeros(frame_num,24,3)], 'tran': [torch.zeros(frame_num,3)]},
+               "/home/chqian/Project/PIP/PIP/data/dataset_work/UWB_IMU/Dataset_B_not_align/test.pt")
+    # # Add everything to the scene and display at 30 fps.
     v = Viewer()
     v.playback_fps = 30.0
 
-    v.scene.add(rb)
+    v.scene.add(rb,arr)
     v.run()
     

examples/load_UWB_IMU_results.py
@@ -3,12 +3,11 @@ import pickle as pkl
 
 import torch
 import numpy as np
-
+from pytorch3d.transforms import matrix_to_axis_angle
 from aitviewer.configuration import CONFIG as C
 from aitviewer.models.smpl import SMPLLayer
 from aitviewer.renderables.rigid_bodies import RigidBodies
 from aitviewer.renderables.smpl import SMPLSequence
-from aitviewer.renderables.arrows import Arrows
 from aitviewer.viewer import Viewer
 
 if __name__ == "__main__":
@@ -18,22 +17,19 @@ if __name__ == "__main__":
     # with open(r"/home/chqian/Project/PIP/PIP/data/dataset_work/UWB_IMU/Dataset_B/test.pt", "rb") as f:
     #     data = pkl.load(f, encoding="latin1")
     
-    path = "/home/chqian/Project/PIP/PIP/data/dataset_work/UWB_IMU/Dataset_B/test.pt"
-    #path = "/home/chqian/Project/PIP/PIP/data/dataset_work/TotalCapture/test.pt"
-    #path = "/home/chqian/Project/PIP/PIP/data/dataset_work/DIP_IMU/test.pt"
+    #path = "/home/chqian/Project/PIP/PIP/data/result/Dataset_B/PIP/0.pt"
+    
+    path = "/home/chqian/Project/PIP/PIP/data/result/Dataset_B_not_align/PIP/0.pt"
+    
     data = torch.load(path)
 
     # Whether we want to visualize all 17 sensors or just the 6 sensors used by DIP.
     all_sensors = False
+    uwb_imu_rot = np.array([[1, 0, 0], [0, 0, 1.0], [0, -1, 0]])
     
     # Get the data.
-    poses = data["pose"][0].view(-1,72)
-    tran = data["tran"][0].view(-1,3)
-    oris = data["ori"][0].numpy().reshape(-1,6,3,3)
-    accs = data["acc"][0].numpy().reshape(-1,6,3)
-    
-    # v_oris = data["vori"][0].numpy().reshape(-1,6,3,3)
-    # v_oris = v_oris[::2]
+    poses = matrix_to_axis_angle(data[0]).view(-1,72)
+    tran = data[1].view(-1,3)
     
     
     # Subject 6 is female, all others are male (cf. metadata.txt included in the downloaded zip file).
@@ -41,9 +37,7 @@ if __name__ == "__main__":
 
     # Downsample to 30 Hz.
     poses = poses[::2]
-    oris = oris[::2]
     tran = tran[::2]
-    accs = accs[::2]
 
     # DIP has no shape information, assume the mean shape.
     betas = torch.zeros((poses.shape[0], 10)).float().to(C.device)
@@ -79,15 +73,7 @@ if __name__ == "__main__":
         "rfoot",
     ]
 
-    # We manually choose the SMPL joint indices cooresponding to the above sensor placement.
-    joint_idxs = [20, 21, 4, 5, 15, 0]
-    
-    # Select only the 6 input sensors if configured.
-    sensor_sub_idxs = [7, 8, 11, 12, 0, 2] if not all_sensors else list(range(len(joint_idxs)))
-    rbs = RigidBodies(joints[:, joint_idxs].cpu().numpy(), oris)
-    
 
-    arr = Arrows(origins=joints[:, joint_idxs].cpu().numpy(),tips = joints[:, joint_idxs].cpu().numpy() - accs * 1/30)
     #rbs_v = RigidBodies(joints[:, joint_idxs].cpu().numpy(), v_oris,color=(0,0,0,1))
 
     # Display the SMPL ground-truth with a semi-transparent mesh so we can see the IMUs.
@@ -98,5 +84,5 @@ if __name__ == "__main__":
     v = Viewer()
     v.playback_fps = 30.0
 
-    v.scene.add(smpl_seq, rbs, arr)
+    v.scene.add(smpl_seq)
     v.run()